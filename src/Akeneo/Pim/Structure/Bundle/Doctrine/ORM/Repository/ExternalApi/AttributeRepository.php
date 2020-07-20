0022a6482f35aa3e205d05ac004f748d540343d3
Willy <willy.mesnage@akeneo.com>
2020-07-20 16:15:40 +0200

    API-1232: update attribute repo validation

--- src/Akeneo/Pim/Structure/Bundle/Doctrine/ORM/Repository/ExternalApi/AttributeRepository.php
+++ src/Akeneo/Pim/Structure/Bundle/Doctrine/ORM/Repository/ExternalApi/AttributeRepository.php
@@ -166,7 +166,16 @@ class AttributeRepository extends EntityRepository implements AttributeRepositor
                         ])
                     ],
                 ])
-            ])
+            ]),
+            'updated' => new Assert\All([
+                new Assert\Collection([
+                    'operator' => new Assert\IdenticalTo([
+                        'value' => Operators::GREATER_THAN,
+                        'message' => 'Searching on the "updated" property require the ">" (greater than) operator, {{ compared_value }} given.',
+                    ]),
+                    'value' => new Assert\DateTime(),
+                ])
+            ]),
         ];
 
         $exceptionMessage = '';
@@ -178,7 +187,6 @@ class AttributeRepository extends EntityRepository implements AttributeRepositor
                     $property
                 ));
             }
-
             $violations = $validator->validate($searchFilter, $constraints[$property]);
             if (0 !== $violations->count()) {
                 foreach ($violations as $violation) {
